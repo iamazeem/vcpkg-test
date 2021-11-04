ad327b2723cb2a7af5f380c8e53489b7a9c69e56
Azeem Sajid <azeem.sajid@gmail.com>
2021-11-04 18:40:12 +0500

    Removed Windows from CI.

--- scripts/build-and-run.sh
+++ scripts/build-and-run.sh
@@ -4,6 +4,9 @@ set -e
 
 BUILD_DIR="${1:-build}"
 
+echo "Removing existing build directory [$BUILD_DIR]..."
+rm -rf "$BUILD_DIR"
+
 VCPKG_CMAKE="$PWD/vcpkg/scripts/buildsystems/vcpkg.cmake"
 echo "Validating vcpkg..."
 if [[ ! -f $VCPKG_CMAKE ]]; then
@@ -11,8 +14,7 @@ if [[ ! -f $VCPKG_CMAKE ]]; then
   exit 1
 fi
 
-echo "Removing existing build directory [$BUILD_DIR]..."
-rm -rf "$BUILD_DIR"
+echo "CMAKE_TOOLCHAIN_FILE=$PWD/vcpkg/scripts/buildsystems/vcpkg.cmake"
 
 echo "Building..."
 cmake -B "$BUILD_DIR" -S . -DCMAKE_TOOLCHAIN_FILE="$PWD/vcpkg/scripts/buildsystems/vcpkg.cmake"
